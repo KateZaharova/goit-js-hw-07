46fb5b19d6ed03206f013be008b4b942cc3c2fe3
Kate Zaharova <15071981ktz@gmail.com>
2023-06-25 19:50:28 +0300

    task-01

---
+++ js/01-gallery.js
@@ -0,0 +1,61 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+
+
+const galleryList = document.querySelector(".gallery");
+
+const markup = createGalleryItemsMarkup(galleryItems);
+
+function createGalleryItemsMarkup(items) {
+    return items
+        .map(({ preview, original, description }) => {
+            return `<li class="gallery__item">
+            <a class="gallery__link" href='${original}'>
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source='${original}'
+                    alt="${description}"
+                />
+            </a>
+        </li>`})
+        .join('');      
+}
+
+galleryList.insertAdjacentHTML('beforeend', markup);
+
+galleryList.addEventListener("click", handlerClick);
+
+function handlerClick(event) {
+   event.preventDefault();
+  if (event.target.classList.contains("gallery__image")) {
+      console.log(event.target.dataset.source)
+      instanceLB.element().querySelector('img').src = event.target.dataset.source;
+      instanceLB.show();
+    }
+} 
+
+const instanceLB = basicLightbox.create('<img />', 
+{
+    onShow: (instanceLB) => {
+      window.addEventListener('keydown', closeOnEsc);
+    },
+    onClose: (instanceLB) => {
+      window.removeEventListener('keydown', closeOnEsc);
+    },
+  }
+)
+
+function closeOnEsc(event) {
+    if (event.code === "Escape") {
+        instanceLB.close()
+    }
+};
+
+
+
+
+
+
+
